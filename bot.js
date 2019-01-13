8dfb914058a455319880316a0d5627a3601018c6
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-13 20:11:25 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Lol..")//حط اسم السرفر مكان eyad..
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'chat');// حط اسم الروم بدل codes
+    if (!channel) {
+        console.log("i have Error !!");
+        return;
+    }
+			 	         var currentTime = new Date(),
+		  hours = currentTime.getHours() + 4 ,
+          hours2 = currentTime.getHours() + 1 ,             
+		   minutes = currentTime.getMinutes(),             
+		   seconds = currentTime.getSeconds(),
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate();
+             if(hours2 > 12) {
+               hours2 -= 12;
+            } else if(hours2 == 0) {
+                hours2 = "12";
+            
+            }  
+            var suffix = 'AM';
+            if (hours >= 12) {
+                suffix = 'PM';
+                hours = hours - 12;	
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+         var ee = member.user;
+		     var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Lol..")//حط اسم السرفر مكان eyad..
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ee.send(`We Thank ${Invite.inviter}\nFor he has brought you into the server \nYou are logged in from this link https://discord.gg/${Invite.code}`);
+ channel.send({embed : new Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setThumbnail(ee.avatarURL)
+       .setTitle(`**New Member !! **`)	
+       .setDescription(`1- Invited By : ${Invite.inviter} \n2- Link Invited : https://discord.gg/${Invite.code} \n3- Member Name : ${member} \n4- Member ID : ${member.id} \n5- Data : ${Day}-${Month}-${Year} \n6- Time : ${hours2}:${minutes}:${seconds} ${suffix}`)
+	   });
+            dat[Inv] = Invite.uses;
+ }
+         })
+    })
+
+	});
+  client.login(process.env.BOT_TOKEN);
